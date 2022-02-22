75fed037b8d7939e7e12b477e7df14819be07698
yongbing <1711401757@qq.com>
2022-02-22 21:50:55 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
 
 createApp(App).use(store).use(router).mount('#app')
